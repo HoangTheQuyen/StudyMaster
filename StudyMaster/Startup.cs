49771e3866a88e36f14e1e0dd0dbc0570895f88b
HoangTheQuyen <38252060+HoangTheQuyen@users.noreply.github.com>
2018-09-26 08:19:13 +0700

    Sườn Đồ Án
    Sườn web luyện thi lớp 10

--- StudyMaster/Startup.cs
+++ StudyMaster/Startup.cs
@@ -15,20 +15,18 @@ namespace StudyMaster
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(route =>
+            route.MapRoute(name: "Default",
+                template: "{Controller=Home}/{Action=Index}/{Id?}"));
         }
     }
 }
